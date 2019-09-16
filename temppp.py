daa9c95e980b86af8da8db3936ba92a23fb9b6b5
HUSTHY <840499869@qq.com>
2019-09-16 14:22:41 +0800

    辅助

---
+++ temppp.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+# -*- encoding: utf-8 -*-
+
+'''
+@Author  :    HY
+@Software:   PyCharm
+@File    :   temppp.py
+@Time    :   2019/9/16 14:21
+@Desc    :
+
+'''
+# SELECT * FROM patent_college WHERE companyId='30bce2146c815640920c8af5b78edf39' AND companyName='华中科技大学'
