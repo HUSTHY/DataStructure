336e475aba70ad413f3b5422afa9aedb5947ee1c
HUSTHY <840499869@qq.com>
2019-09-17 11:01:05 +0800

    基础的数据结构python的实现： list、队列、栈、树、哈夫曼树等

--- temppp.py
+++ temppp.py
@@ -11,3 +11,5 @@
 
 '''
 # SELECT * FROM patent_college WHERE companyId='30bce2146c815640920c8af5b78edf39' AND companyName='华中科技大学'
+
+select * from patent where patentId IN (select patentId from patent where isValid=1 AND checkStatus in ("有效", "无效", "待定") AND companyId in (SELECT companyId from confParkCompany where  env="prod" and parkName="华中科技大学科技园创新基地" and isValid=1))
